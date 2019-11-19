33bbad81eb8d2438fced0033f2c70f9d846cb2a7
Streaked <omborkar@localhost.localdomain>
2019-11-19 18:31:15 -0500

    "Made a static folder"

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ const keys = require('./config/keys')
 
 
 app.set('view engine', 'ejs')
+app.use(express.static('static'))
 app.use(express.urlencoded());
 app.use(flash());
 app.use(session({
     secret: keys.secret,
     resave: false,
     saveUninitialized: false,
+    cookie:{
+        expires: true
+
+    }
 }))
 
 app.use('/login', require('./routes/login'))
@@ -21,6 +26,9 @@ app.use('/register', require('./routes/register'))
 
 app.get('/', (req, res)=>{
     res.render('index')
+})
+app.get('/session', (req,res)=>{
+    console.log(req.session);
 })
 
 app.listen(PORT, ()=>{
--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-// const passport = require('passport')
-// const initializePassport = require('../config/passport');
-
-
+const MongoClient = require('mongodb');
+const session = require('express-session');
 
 
 
 router.get('/', (req, res)=>{
-    // initializePassport(passport)
     res.render('login');
     
 })
 router.post('/userLogin', (req, res)=>{
-    // find the user through a email address and store it into a user object
-    
-    initializePassport(passport)
+   const User = {
+       username : req.body.username,
+       password : req.body.password
+   }
+   req.session.name = "Om";
+   console.log(req.session)
 })
 
 router.get('/logout', (req, res)=>{
